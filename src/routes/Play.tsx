64d22935560cb7acbbd559d1fff03e7a9edd1725
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2024-08-05 18:37:56 +0100

    🚑 Update file name casing

---
+++ src/routes/Play.tsx
@@ -0,0 +1,20 @@
+import { AppShell } from '@mantine/core'
+import Map from 'src/components/Map'
+import Sidebar from 'src/components/Sidebar'
+
+import { useGameStore } from '~/state/useGameStore'
+
+export default function Play() {
+	const location = useGameStore(state => state.location)
+
+	return location ? (
+		<AppShell navbar={{ width: 300, breakpoint: 'sm' }}>
+			<AppShell.Navbar>
+				<Sidebar />
+			</AppShell.Navbar>
+			<AppShell.Main>
+				<Map />
+			</AppShell.Main>
+		</AppShell>
+	) : null
+}
